75c755bbf5b582fac16113691c8785d3de532d50
adamschutte41 <adamschutte41@gmail.com>
2020-10-07 17:58:43 -0400

    Checkpoint 3

--- app/src/main/java/com/mobiledev/ourapp/whatsfordinner/MainActivity.java
+++ app/src/main/java/com/mobiledev/ourapp/whatsfordinner/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +15,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d(TAG, "onCreate() called");
+
+        Button eatOutBtn = findViewById(R.id.eatOutBtn);
 
     }
 
