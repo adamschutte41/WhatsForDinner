dc5819012a320af510e31684cba11a4bc9ff4215
adamschutte41 <adamschutte41@gmail.com>
2020-10-07 15:51:40 -0400

    Adding lifecycle functions

--- app/src/main/java/com/mobiledev/ourapp/whatsfordinner/MainActivity.java
+++ app/src/main/java/com/mobiledev/ourapp/whatsfordinner/MainActivity.java
@@ -3,12 +3,47 @@ package com.mobiledev.ourapp.whatsfordinner;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate() called");
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
     }
 }
