f522a37346cfecd83b82c1395c4f93c464535d0a
Max-Grelson <maxgrelson@gmail.com>
2020-11-12 14:22:13 -0800

    Added Credential change functionality

---
+++ app/src/main/java/com/mobiledev/ourapp/whatsfordinner/LoginSettingsActivity.java
@@ -0,0 +1,62 @@
+package com.mobiledev.ourapp.whatsfordinner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class LoginSettingsActivity extends AppCompatActivity {
+
+    User user;
+    EditText username;
+    EditText password;
+    EditText newUsername;
+    EditText newPassword;
+    TextView submit;
+
+    public void onSubmit(View view){
+
+
+        String userIn = username.getText().toString();
+        String passIn = password.getText().toString();
+        String newUserIn = newUsername.getText().toString();
+        String newPassIn = newPassword.getText().toString();
+
+        user = User.getInstance();
+        user.setUsername(userIn);
+        user.setPassword(passIn);
+        user.setContext(getApplicationContext());
+
+        long result = user.getUser(userIn, passIn);
+
+        if(result != -1){
+            user.editUser(userIn, passIn, newUserIn, newPassIn);
+            Toast.makeText(getApplicationContext(), "CREDENTIALS CHANGED", Toast.LENGTH_LONG).show();
+            startActivity(new Intent(LoginSettingsActivity.this, MainActivity.class));
+        }else {
+            Toast.makeText(getApplicationContext(), "ERROR", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_settings);
+
+
+        Button submitBtn = findViewById(R.id.submitBtn);
+        submit = findViewById(R.id.submitTextView);
+
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+
+        newUsername = findViewById(R.id.newUsername);
+        newPassword = findViewById(R.id.newPassword);
+
+    }
+}
